64eaa8f15e5f03e34592ea99265ed669676562ec
elieldiniz <elielfantichelle@outlook.com.br>
2024-12-25 12:36:25 -0300

    first commit

---
+++ apps/lojaonline/next.config.ts
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+      domains: ['firebasestorage.googleapis.com'], // Adicione o domínio aqui
+  },
+}
+
+module.exports = nextConfig;
---
+++ apps/lojaonline/src/app/(paginas)/layout.tsx
@@ -0,0 +1,8 @@
+import Pagina from '@/components/template/Pagina'
+
+interface LayoutProps {
+    children: React.ReactNode; 
+}
+export default function Layout(props: LayoutProps) {
+    return <Pagina>{props.children}</Pagina>
+}
